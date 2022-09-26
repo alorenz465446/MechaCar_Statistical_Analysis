7a6111ea1d32a8f3d1ac7790e5704d6f3b4838d7
alorenz465446 <107652317+alorenz465446@users.noreply.github.com>
2022-09-26 18:11:30 -0400

    analysis of mpg data

---
+++ MechaCarChallenge.R
@@ -0,0 +1,27 @@
+library(dplyr)
+
+
+### Deliverable 1 ###########
+# import csv
+MechaCar_mpg <- read.csv(file='./Data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+# perform linerar regression with lm() function
+Mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+Mecha_lm
+
+# view summary
+summary(Mecha_lm)
+
+# view r squared
+summary(Mecha_lm)$r.squared
+
+
+### Deliverable 2 ########
+# import csv2
+Suspension_Coil <- read.csv(file='./Data/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+
+# summarize total data with summarize () function
+total_summary <- Suspension_Coil %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
+total_summary
